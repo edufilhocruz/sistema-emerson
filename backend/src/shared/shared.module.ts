8f45f46c4f42ff6e5ea770f00037110638a21d54
Eduardo Cruz <eduardocruz@fexol.com.br>
2025-08-14 13:31:29 -0300

    fix: adicionar ImagePreviewService ao SharedModule para injeção de dependência
    - Adicionar ImagePreviewService aos providers e exports do SharedModule
    - Corrigir problema de injeção de dependência que impedia o salvamento de imagens
    - Garantir que o ImagePreviewService seja disponível para todos os módulos

--- backend/src/shared/shared.module.ts
+++ backend/src/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TemplateEngineService } from './services/template-engine.service';
 import { EmailTemplateService } from './services/email-template.service';
 import { FileManagerService } from './services/file-manager.service';
+import { ImagePreviewService } from './services/image-preview.service';
 import { TemplateDemoController } from './controllers/template-demo.controller';
 
 @Module({
@@ -10,11 +11,13 @@ import { TemplateDemoController } from './controllers/template-demo.controller';
     TemplateEngineService,
     EmailTemplateService,
     FileManagerService,
+    ImagePreviewService,
   ],
   exports: [
     TemplateEngineService,
     EmailTemplateService,
     FileManagerService,
+    ImagePreviewService,
   ],
 })
 export class SharedModule {}
